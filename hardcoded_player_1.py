3a4d88744e883957a686b8b4aac740e33bb4bfb3
Cihan Bosnalı <cbosnali21@my.sevkoleji.k12.tr>
2021-05-29 16:44:10 +0300

    Deleted forgotten code and added comments

--- hardcoded_player_1.py
+++ hardcoded_player_1.py
@@ -2,43 +2,64 @@ import requests
 import json
 import math
 
+# Yarışmadan veri aldığınız adres 
 get_url = 'http://localhost:3000'
+
+# Yarışmaya veri gönderdiğiniz adres
 post_url = 'http://localhost:4000'
+
+# Verilerin JSON tipinde olduğu belirten iki satır
 get_headers = {'Content-Type': 'application/json'}
 post_headers = {'Content-Type': 'application/json'}
 
+# Tankın gitmesini istediğimiz lokasyonlar (x,y) listesi
 positions2go = [[110, 370],[320,370],[320,620],[680,620],[850,320]]
-current_pos = []
 
+# Oyun boyunca sürekli yapmasını istediklerimiz
 while True:
+    # Oyundan veriyi çek 
     data = requests.get(get_url, headers=get_headers)
+    # Veriyi JSON'dan kullanılabilir hale getir
     jsonData = json.loads(data.text)
 
+    # Veriyi değişkenlere ata
     current_x = float(jsonData["tankA"]["x"])
     current_y = float(jsonData["tankA"]["y"])
     current_r = float(jsonData["tankA"]["r"])
 
+    # Lokasyonlar listesi dolu oldukça bunları yap
     if len(positions2go) > 0:
+        # Denerken görmek için veriyi yazdır (gidilecek lokasyon, uzaklık x ve y)
         print(positions2go[0], abs(positions2go[0][0] - current_x), abs(positions2go[0][1] - current_y))
+        
+        # Eğer lokasyona 30 pixelden uzaktaysak bunu yap
         if abs(positions2go[0][0] - current_x) > 30 or abs(positions2go[0][1] - current_y) > 30:
+            # atan matematik fonksiyonuyla dönmen gereken açıyı bul
             wanted_r_rad = math.atan2((positions2go[0][1] - current_y),(positions2go[0][0] - current_x))
-            # atan2 is arctan but it chooses the right quadrant by itself. Returns between PI and -PI.
+            # PI ve -PI arasında bulduğun açıyı dereceye çevir.
             wanted_r = -((wanted_r_rad * 180) / math.pi)
+            # Açıya 30 dereceden fazla yaklaşana kadar dön.
             if abs(current_r - wanted_r) <= 30:
                 m, r = 1, 0
+            # Doğru açıdasın ilerle
             else:
                 m, r = 0, 1
+        # Listedeki lokasyona varınca bunu yap
         else:
+            # varılan lokasyonu listeden çıkart
             positions2go.pop(0)
             m, r = 0, 0
     else:
+        # Lokasyonlar listesi boşalınca bunu yap
         m, r = 0, 0
         print("ERROR: No position specified to go.")
 
+    # Ateş etme
     f = 0
 
+    # Veriyi göndermek için hazırla
     data = {"m": m, "r":r, "f":f}
+    # JSON formatına dönüştür
     data = json.dumps(data)
+    # Gönder
     response = requests.post(post_url, data=data, headers=post_headers)
-
-
